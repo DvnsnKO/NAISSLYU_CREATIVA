43143dcc413faec042c41106a9a2c6c8c302c261
DvnsnKO <da-v-inson@hotmail.com>
2024-06-04 18:51:09 -0500

    CRUD MONTADO-reordenamiento de carpetas

--- index.php
+++ index.php
@@ -1,36 +1,29 @@
+<!-- Arma lo que se muestra en la ventana de inicio  -->
+
 <?php
 
 
-require "./controlador/controlador.php";
-require "./modelo/modelo.php";
+
 
 
 if (isset($_GET["ruta"])) {
-    include('./vistas/cliente/header.vistas.php');
-    include('./vistas/cliente/anuncios.vistas.php');
-    include('./vistas/cliente/subheader.vistas.php');
+    require "./app/controlador/vistas/vistas_cliente.controlador.php";
+require "./app/modelo/vistas/vistas_cliente.modelo.php";
+    include('./public/vistas/cliente/header.vistas.php');
+    include('./public/vistas/cliente/anuncios.vistas.php');
+    include('./public/vistas/cliente/subheader.vistas.php');
     $rutas = new RutasControlador();
     $rutas->Rutas();
-    include('./vistas/cliente/footer.vistas.php');
-
-} else {
-    
-    include_once('menuinicio.vistas.php');
+    include('./public/vistas/cliente/footer.vistas.php');
 
 } 
+/* Se cambio la "ruta" para que no contenga nada al principio y ejecute la ventana de OPCIONES.PHP 
+que enruta a las vistas del admin y cliente */
+else {
+    
+    include_once('opciones.php');
 
-
-
-
-
-
-
-
-
-
-
-//poner controlador aqui
-
+} 
 
 
 
--- menuinicio.vistas.php
+++ menuinicio.vistas.php
@@ -9,53 +9,50 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="./public/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="./dist/css/adminlte.css">
-  <link rel="stylesheet" href="./dist/css/naisslyu.css">
+  <link rel="stylesheet" href="./public/dist/css/adminlte.css">
+  <link rel="stylesheet" href="./public/dist/css/naisslyu.css">
 
 
 </head>
 
+
+
 <body>
 
-  <div class="container mt-4">
-    <div class="row">
-      <div class="col-12">
-        <div class="card card-primary">
-          <div class="card-header">
-            <h3 class="card-title">PANTALLA TEMPORAL, SOLO PARA EVIDENCIAS</h3>
-          </div>
-          <!-- /.card-header -->
-        
-            <div class="row mt-4">
-              <div class="col-6">
-
-                <a href="index.php?ruta=destacados">
-
-                  <img src="./images/usuario.jpg" alt="Photo 1" height="100%" width="100%">
-                  <button class="col-12 btn btn-primary">usuario</button>
-
-                </a>
-              </div>
-
-              <div class="col-6 ">
-                <a href="admin">
-
-                  <img src="./images/administrador.jpg" alt="Photo 2" height="100%" width="100%">
-                  <button class="col-12 btn btn-primary">administrador</button>
-
-                </a>
-              </div>
-
-            </div>
-         
-        </div>
+  <form action="" method="GET">
+    <div class="form-group row">
+      <div class="col">
 
 
-
+        <input type="number" id="numero" name="numero" class="form-control" placeholder="digite el numero del puerto">
+      </div>
+      <div class="col-auto">
+        <button type="submit" class="btn btn-success">Enviar</button>
       </div>
     </div>
-  </div>
+  </form>
+  <?php
+ 
+ 
+  
+ 
+ 
+    
+require_once "conexion.php";
+    
+    
+    
+    
+
+    
+    
+    
+
+
+  ?>
 </body>
+
+
 </html>
